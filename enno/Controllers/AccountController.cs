c6e47fa3df19cf611cdaf1ae2a2f1a5e60617f75
AytenZeynalova <14rvxpg@code.edu.az>
2022-06-29 02:05:24 +0400

    first commit

---
+++ enno/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using enno.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace enno.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager )
+        {
+            _userManager = userManager;
+        }
+        //public async Task<IActionResult> CreateAdmin()
+        //{
+        //    AppUser admin = new AppUser
+        //    { 
+        //        FullName="super admmin",
+        //        UserName="superAdmin",
+        //    };
+        //    var result= await _userManager.CreateAsync(admin, "Admin123");
+
+        //    if (!result.Succeeded)
+        //    {
+        //        return Ok(result.Errors);
+
+        //    }
+
+        //    return View();
+        //}
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ enno/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using enno.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace enno.DAL
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        DbSet<AppUser> Users { get; set; }
+    }
+}
